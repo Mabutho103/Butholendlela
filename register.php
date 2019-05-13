ab2d48b5028e66b2ba2eaa88aa154cae6f9d3ff9
Mabutho103 <50584541+Mabutho103@users.noreply.github.com>
2019-05-13 15:55:23 +0200

    Add files via upload

---
+++ register.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+</script>
+
+	<title></title>
+</head>
+<body style="background:white;color:black">
+ <div class="container">
+
+	
+
+
+<br>
+
+ </div>
+<center><H1>Registration</H1></center>
+<hr width="100%" color="#000" size="4"/>
+<h3>Fill in your information here</h3>
+
+<div>
+	<form class="needs-validation" novalidate>
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="validationTooltip01">First name</label>
+      <input type="text" class="form-control" id="validationTooltip01" placeholder="First name" required>
+      <div class="valid-tooltip">
+        <br>
+      </div>
+    </div>
+    <div class="col-md-4 mb-3">
+      <label for="validationTooltip02">Last name</label>
+      <input type="text" class="form-control" id="validationTooltip02" placeholder="Last name" required>
+      <div class="valid-tooltip">
+      <br>
+
+      </div>
+    </div>
+    <div class="col-md-4 mb-3">
+      <label for="validationTooltipUsername">Username</label>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+        </div>
+        <input type="text" class="form-control" id="validationTooltipUsername" placeholder="Username" aria-describedby="validationTooltipUsernamePrepend" required>
+        <div class="invalid-tooltip">
+          <br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationTooltip03">City</label>
+      <input type="text" class="form-control" id="validationTooltip03" placeholder="City" required>
+      <div class="invalid-tooltip">
+       <br>
+      </div>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationTooltip04">Province</label>
+      <input type="text" class="form-control" id="validationTooltip04" placeholder="Province" required>
+      <div class="invalid-tooltip">
+      <br>
+      </div>
+    </div>
+ 
+    </div>
+  </div>
+  <button class="btn btn-danger" type="submit">Submit form</button>
+</form>
+
+<form method= post action="/cgi=bin/example.cgi">
+
+<br>
+
+We will remember your preference.  
+
+<br>
+
+<form>
+  <div class="form-row align-items-center">
+    <div class="col-auto my-1">
+      <label class="mr-sm-2" for="inlineFormCustomSelect">Select your language.</label>
+      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+        <option selected>IsiZulu </option>
+        <option value="1">English</option>
+        <option value="2">IsiXhosa</option>
+        <option value="3">SeTswana</option>
+      </select>
+    </div>
+    <div class="col-auto my-1">
+      <div class="custom-control custom-checkbox mr-sm-2">
+        <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+        <label class="custom-control-label" for="customControlAutosizing">Remember my preference</label>
+      </div>
+    </div>
+     <br>
+     <br>
+    <div class="col-auto my-1">
+     
+    </div>
+  </div>
+</form>
+
+<br>
+<br>
+	</form>
+</form>
+
+
+</div>
+
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+<a href="index.php">Home</a>
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+</body>
+</html>
